722606ac85f02f35299db2fbbc3966286efbf004
Admin <Admin@DESKTOP-1SLOCA9>
2020-07-06 17:51:56 +0900

    200706

--- _2020_07_06/AbstractClass/Employee.h
+++ _2020_07_06/AbstractClass/Employee.h
@@ -2,19 +2,20 @@
 #include <iostream>
 using namespace std;
 
-//순수 가상함수 1개이상 있으면
-//추상클래스(Abstract class)
-//이 클래스는 객체를 만들 수 없다 
-//상속을 전제로 한다
+/* 순수 가상 함수가 1개이상이 있으면
+추상 클래스(Abstract Class)라 부른다
+이 클래스는 객체를 만들 수 없다
 
+무조건 상속을 전제로 한다
+*/
 class Employee
 {
 private:
 	string name;
 	string companyNum;
 public:
-	void inputEmployee();
-	void showEmployee();
-	virtual void calcpay() = 0;// 순수 가상 함수
+	virtual void inputEmployee();
+	virtual void showEmployee();
+	virtual int calcPay() = 0;	// 순수 가상 함수
 };
 
---
+++ _2020_07_06/AbstractClass/EmployeeManager.cpp
@@ -0,0 +1,21 @@
+#include "EmployeeManager.h"
+#include <iostream>
+
+using namespace std;
+
+void EmployeeManager::showMenu()
+{
+
+}
+int EmployeeManager::getSelNum()
+{
+
+}
+void EmployeeManager::insertEmployee()
+{
+
+}
+void EmployeeManager::showAllEmployee()
+{
+
+}
